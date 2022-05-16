ce57880773d265c17011c34f2f9acfe082b077dd
Wachira11ke <100116527+Wachira11ke@users.noreply.github.com>
2022-05-16 19:39:09 +0300

    This is a combination of 4 commits.
    Rename a Commit
    
    First Code Correction
    
    Second Code Correction
    
    Third Code Correction

--- insert.php
+++ insert.php
@@ -1,10 +1,6 @@
 <?php
-
-// Use ls command to shell_exec
-// function
+// Use ls command to shell_exec function
 $output = shell_exec('git');
-
-// Display the list of all file
-// and directory
+// Display the list of all files and directories
 echo "<pre>$output</pre>";
 ?>
